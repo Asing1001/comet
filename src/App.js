7192fc72939b9749cbb129ca071d410600bcb0b9
AndyChen <shinningstar1001@gmail.com>
2017-07-24 00:13:59 +0800

    improve ui and put styles in css file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const articles = [{ title: 123, voteCount: 0 }, { title: 888, voteCount: 5 }, { title: 456, voteCount: 88 }];
+const articles = [
+  { title: 'Hello world', voteCount: 0 },
+  { title: 'Wow! \nReact version is awesome!!', voteCount: 5 },
+  { title: 'I love coding', voteCount: 88 }
+];
 
 const Article = ({article, onChangeVote}) => {
   return (
-    <div style={{ display: 'flex', alignItems: 'center', padding: '.5em 0em', border: '1px solid', margin: '.3em 0em' }}>
-      <div style={{ width: '100px', textAlign: 'center' }}>
+    <article className="article">
+      <div className="counter">
         <div><i className="fa fa-arrow-up" onClick={() => onChangeVote(1)}></i></div>
         <div>{article.voteCount}</div>
         <div><i className="fa fa-arrow-down" onClick={() => onChangeVote(-1)}></i></div>
       </div>
-      <div style={{ 'flex': 1 }}>{article.title}</div>
-    </div>)
+      <p className="content">{article.title}</p>
+    </article>)
 }
 
 class App extends Component {
@@ -25,11 +29,11 @@ class App extends Component {
 
   }
 
-  handleChange = (event) => {
+  handleInputChange = (event) => {
     this.setState({ newTitle: event.target.value })
   }
 
-  addArticle = (event) => {
+  articleSubmit = (event) => {
     this.state.articles.unshift({ title: this.state.newTitle, voteCount: 0 })
     this.setState({ articles: this.state.articles })
     event.preventDefault();
@@ -43,11 +47,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Comet</h2>
-        </div>
-        <form onSubmit={this.addArticle}>
-          <input type="text" value={this.state.newTitle} onChange={this.handleChange.bind(this)} maxLength="255" required />
+        <h2>Welcome to Comet</h2>
+        <form onSubmit={this.articleSubmit} className="article-form">
+          <textArea rows="3" cols="100" type="text" value={this.state.newTitle} onChange={this.handleInputChange.bind(this)} maxLength="255" required />
           <button type="submit" >Add Article</button>
         </form>
         {this.state.articles.sort(({voteCount: a}, {voteCount: b}) => b - a).slice(0, 20).map((article, index) =>
