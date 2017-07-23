7fe32819c800a8ab3f843f02b035374c5473d6cf
AndyChen <shinningstar1001@gmail.com>
2017-07-23 22:13:14 +0800

    Add article create and listing

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const articles = [{ title: 123 }, { title: 888 }, { title: 456 }];
+
+const Article = ({article}) => {
+  return (<div>{article.title}</div>)
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTitle: '',
+      articles
+    };
+
+  }
+
+  handleChange = (event) => {
+    this.setState({ newTitle: event.target.value })
+  }
+
+  addArticle = (title) => {
+    this.state.articles.push({ title })
+    this.setState({ articles: this.state.articles })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Comet</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" value={this.state.newTitle} onChange={this.handleChange.bind(this)} />
+        <button onClick={() => this.addArticle(this.state.newTitle)}>Add Article</button>
+        {this.state.articles.map((article, index) =>
+          <Article article={article} key={index}></Article>
+        )}
       </div>
     );
   }
