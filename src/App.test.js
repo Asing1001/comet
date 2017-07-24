dba6e3e77a5b23163896cb601182fcf84a372d47
AndyChen <shinningstar1001@gmail.com>
2017-07-24 12:58:46 +0800

    Add testing for top 20 and testing for order

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Topic from './Topic';
 
 describe('Component:App', () => {
@@ -38,5 +38,49 @@ describe('Component:App', () => {
     //assert
     const actual = wrapper.state('topics').length;
     expect(actual).toEqual(expectResult);
+  });
+
+
+
+  it('should only show 20 topic', () => {
+    //arrange
+    const wrapper = shallow(
+      <App />
+    );
+    const expectResult = 20;
+    const form = wrapper.find('form');
+
+    //act
+    for (var i = 0; i < 30; i++) {
+      form.simulate('submit', { preventDefault() { } });
+    }
+
+    //assert
+    const actual = wrapper.find('Topic').length;
+    expect(actual).toEqual(expectResult);
+  });
+
+
+
+  it('should sort from big to small', () => {
+    //arrange
+    const wrapper = shallow(
+      <App />
+    );
+    const topics = [
+      { title: 'Hello world', voteCount: 2 },
+      { title: 'Wow! \nReact version is awesome!!', voteCount: 4 },
+      { title: 'I love coding', voteCount: 3 },
+      { title: 'testing title', voteCount: 1 }
+    ];
+
+    //act
+    wrapper.setState({ topics })    
+
+    //assert
+    expect(topics[0].voteCount).toEqual(4);
+    expect(topics[1].voteCount).toEqual(3);
+    expect(topics[2].voteCount).toEqual(2);
+    expect(topics[3].voteCount).toEqual(1);
   });
 })
